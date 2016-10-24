c2922ea98cd3c2618ef410b7ec15c7ceaf306467
Thomas <cruz.thomas65@gmail.com>
2016-10-24 15:12:30 +0200

    mysecondpush

---
+++ src/types/string.js
@@ -0,0 +1,69 @@
+ var string = "Hello World";
+
+ string+=" !";
+ console.log('string : ', string);
+
+ var index = string.indexOf('lo');
+ console.log('index of lo : ', index);
+
+ /*function contains(haystasck, needle) {
+
+  var index = haystasck.indexOf(needle)
+
+  if (index>=0) {
+
+  return true
+  }else{
+  return false}
+  }*/
+
+ function contains(haystasck, needle) {
+  return haystasck.indexOf(needle)>=0;
+
+ }
+
+ var isTrue = contains('jack','ack');
+ var isAlsoTrue = contains('jim','jim');
+ var isFalse = contains('MacDo', 'ack')
+
+
+
+ console.log(isTrue, isAlsoTrue, isFalse);
+ console.log('Jack is back'.includes('ack'));
+
+ /**
+  *Expression régulière
+  */
+
+ var hello = "Hello World";
+ var found = hello.search(/orl/);
+ var alsofound = hello.search(/world/i);
+
+
+ console.log('found : ',found, 'alsofound : ', alsofound);
+
+ // /i for case insensitive : majuscule et minuscule
+ var x = /(.)*(world)/i.test('Hello World');
+ console.log('x : ',x);
+
+ //it is conform : l'objets (.)*(world et bien contenu
+ var strangelyTrue =/(.)*(world)/i.test('Hello World here');
+     console.log('strangelyTrue : ', strangelyTrue);
+
+ var moreStrict =/(.)*(world)$/i.test('Hello World here');
+ console.log('moreStrict : ', moreStrict);
+
+
+ var test = 'Hello world !';
+
+ // substring va imprimer les caractères entre les 2 index donnés
+ //substr va imprimer les caractère entre l'index donné en 1 et le nombre de caractère demandé
+ console.log('substr(2, 3): '   + test.substr(2, 3));
+ console.log('substring(2, 5): ' + test.substring(2, 5));
+//même réponse mais pas même demande
+
+ console.log('substr(-1, 5): '   + test.substr(-1, 4));
+ console.log('substring(-1, 5): ' + test.substring(-1, 4));
+
+ console.log('substr(8, -1): '   + test.substr(8, -1));
+ console.log('substring(8, -1): ' + test.substring(8, -1));
